75021b68210b936c0744410f70a11c84ecb343aa
N-Silbernagel <6422477+N-Silbernagel@users.noreply.github.com>
2026-02-28 22:23:53 +0100

    First working solution

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,41 @@
 
 namespace App;
 
+use DateTimeImmutable;
 use Exception;
 
 final class Parser
 {
+    /**
+     * @throws \DateMalformedStringException
+     */
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        ini_set('memory_limit','16G');
+        $output = [];
+
+        $input = fopen($inputPath, 'r');
+
+        while ($csvRow = fgetcsv($input, escape: '')) {
+            $urlInput = $csvRow[0];
+            $dateInput = $csvRow[1];
+
+            $date = new DateTimeImmutable($dateInput);
+            $parsedUrl = parse_url($urlInput);
+            $path = $parsedUrl['path'];
+
+            $output[$path] ??= [];
+
+            $formattedDate = $date->format('Y-m-d');
+            $output[$path][$formattedDate] ??= 0;
+            $output[$path][$formattedDate]++;
+        }
+
+        foreach ($output as &$data) {
+            ksort($data);
+        }
+
+        $json = json_encode($output, flags: JSON_PRETTY_PRINT);
+        file_put_contents($outputPath, $json);
     }
 }
